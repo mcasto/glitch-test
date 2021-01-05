4f783e6925e56bb53c339d034e883bc819e5e32e
Mike Casto <castoware@gmail.com>
2021-01-05 06:05:57 -0500

    replaced sqlite with nedb

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
+const Datastore = require("nedb-promises");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -15,35 +17,37 @@ app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// init sqlite db
-const dbFile = "./.data/sqlite.db";
-const exists = fs.existsSync(dbFile);
-const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database(dbFile);
+// init db
+const dbFile = "./.data/test.db";
+const db = Datastore.create(dbFile);
 
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(() => {
-  if (!exists) {
-    db.run(
-      "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
-    );
-    console.log("New table Dreams created!");
+// const exists = fs.existsSync(dbFile);
+// const sqlite3 = require("sqlite3").verbose();
+// const db = new sqlite3.Database(dbFile);
 
-    // insert default dreams
-    db.serialize(() => {
-      db.run(
-        'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
-      );
-    });
-  } else {
-    console.log('Database "Dreams" ready to go!');
-    db.each("SELECT * from Dreams", (err, row) => {
-      if (row) {
-        console.log(`record: ${row.dream}`);
-      }
-    });
-  }
-});
+// // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+// db.serialize(() => {
+//   if (!exists) {
+//     db.run(
+//       "CREATE TABLE Dreams (id INTEGER PRIMARY KEY AUTOINCREMENT, dream TEXT)"
+//     );
+//     console.log("New table Dreams created!");
+
+//     // insert default dreams
+//     db.serialize(() => {
+//       db.run(
+//         'INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")'
+//       );
+//     });
+//   } else {
+//     console.log('Database "Dreams" ready to go!');
+//     db.each("SELECT * from Dreams", (err, row) => {
+//       if (row) {
+//         console.log(`record: ${row.dream}`);
+//       }
+//     });
+//   }
+// });
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -52,26 +56,24 @@ app.get("/", (request, response) => {
 
 // endpoint to get all the dreams in the database
 app.get("/getDreams", (request, response) => {
-  db.all("SELECT * from Dreams", (err, rows) => {
-    response.send(JSON.stringify(rows));
-  });
+  db.find({}).then((data) => response.send(JSON.stringify(data)));
 });
 
 // endpoint to add a dream to the database
 app.post("/addDream", (request, response) => {
-  console.log(`add to dreams ${request.body.dream}`);
-
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects
   // so they can write to the database
   if (!process.env.DISALLOW_WRITE) {
     const cleansedDream = cleanseString(request.body.dream);
-    db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, error => {
-      if (error) {
-        response.send({ message: "error!" });
-      } else {
-        response.send({ message: "success" });
-      }
-    });
+    db.insert({ dream: cleansedDream });
+
+    // db.run(`INSERT INTO Dreams (dream) VALUES (?)`, cleansedDream, (error) => {
+    //   if (error) {
+    //     response.send({ message: "error!" });
+    //   } else {
+    //     response.send({ message: "success" });
+    //   }
+    // });
   }
 });
 
@@ -79,33 +81,36 @@ app.post("/addDream", (request, response) => {
 app.get("/clearDreams", (request, response) => {
   // DISALLOW_WRITE is an ENV variable that gets reset for new projects so you can write to the database
   if (!process.env.DISALLOW_WRITE) {
-    db.each(
-      "SELECT * from Dreams",
-      (err, row) => {
-        console.log("row", row);
-        db.run(`DELETE FROM Dreams WHERE ID=?`, row.id, error => {
-          if (row) {
-            console.log(`deleted row ${row.id}`);
-          }
-        });
-      },
-      err => {
-        if (err) {
-          response.send({ message: "error!" });
-        } else {
-          response.send({ message: "success" });
-        }
-      }
-    );
+    db.remove({}, { multi: true });
+    // db.each(
+    //   "SELECT * from Dreams",
+    //   (err, row) => {
+    //     console.log("row", row);
+    //     db.run(`DELETE FROM Dreams WHERE ID=?`, row.id, (error) => {
+    //       if (row) {
+    //         console.log(`deleted row ${row.id}`);
+    //       }
+    //     });
+    //   },
+    //   (err) => {
+    //     if (err) {
+    //       response.send({ message: "error!" });
+    //     } else {
+    //       response.send({ message: "success" });
+    //     }
+    //   }
+    // );
   }
 });
 
 // helper function that prevents html/css/script malice
-const cleanseString = function(string) {
+const cleanseString = function (string) {
   return string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
 };
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
+  console.log(
+    `Your app is running at http://localhost:${listener.address().port}`
+  );
 });
